76472d5464a8946a16d2e2706828ae34b028b059
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-04-18 01:01:17 +0200

    drobne poprawki modelu

--- ATH_UBB/Model/Reservation.cs
+++ ATH_UBB/Model/Reservation.cs
@@ -3,7 +3,7 @@
     public class Reservation : IEntity<Guid>
     {
         public Guid Id { get; set; }
-        public DateTime GetDay { get; set; }
+        public DateTime StartDay { get; set; }
         public DateTime EndDay { get; set; }
         public bool IsReserved { get; set; }
 
